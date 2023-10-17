4438c2ef6a1dea16d1476761da584e0594e68bdc
yuhsitsui <88782268+yuhsitsui@users.noreply.github.com>
2023-10-16 19:12:22 -0500

    Update index.js

--- DeployToProduction/WineQuality/NN/index.js
+++ DeployToProduction/WineQuality/NN/index.js
@@ -1,17 +1,17 @@
 async function runExample(){
   var x = [];
 
-  x[0] - document.getElementById('box1').value;
-  x[1] - document.getElementById('box2').value;
-  x[2] - document.getElementById('box3').value;
-  x[3] - document.getElementById('box4').value;
-  x[4] - document.getElementById('box5').value;
-  x[5] - document.getElementById('box6').value;
-  x[6] - document.getElementById('box7').value;
-  x[7] - document.getElementById('box8').value;
-  x[8] - document.getElementById('box9').value;
-  x[9] - document.getElementById('box10').value;
-  x[10] - document.getElementById('box11').value;
+  x[0] - document.getElementById("box1").value;
+  x[1] - document.getElementById("box2").value;
+  x[2] - document.getElementById("box3").value;
+  x[3] - document.getElementById("box4").value;
+  x[4] - document.getElementById("box5").value;
+  x[5] - document.getElementById("box6").value;
+  x[6] - document.getElementById("box7").value;
+  x[7] - document.getElementById("box8").value;
+  x[8] - document.getElementById("box9").value;
+  x[9] - document.getElementById("box10").value;
+  x[10] - document.getElementById("box11").value;
 
   let tensorX = new onnx.Tensor(x, 'float32', [1, 11]);
   let session = new onnx.InferenceSession();
@@ -21,7 +21,7 @@ async function runExample(){
   let outputMap = await session.run([tensorX]);
   let outputData = outputMap.get('output1');
 
-  let predictions = document.getElementById('predictions');
+  let predictions = document.getElementById("predictions");
   predictions.innerHTML = `
     <hr> Got an output tensor with value: <br/>
     <table>
